7fa4454da2341ce29d6154e1b8d9a107c3defa9b
Ian Haranaka <ian.hrnk@gmail.com>
2021-05-08 12:09:15 -0400

    updates main activity

--- app/src/main/java/com/trabalho/controledecursos/MainActivity.java
+++ app/src/main/java/com/trabalho/controledecursos/MainActivity.java
@@ -1,8 +1,12 @@
 package com.trabalho.controledecursos;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar mainToolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(mainToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        //MenuItem searchItem = menu.findItem(R.id.main_search);
+        //SearchView searchView = (SearchView) searchItem.getActionView();
+
+        // Configure the search info and add any event listeners...
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
